7e89757a3961b66c53a49d467c7dab6f018edb7b
patdanai767 <pattproc@gmail.com>
2024-11-26 21:19:23 +0700

    test-navbar

---
+++ project-alpha/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='flex justify-between bg-slate-500 p-4'>
+      <div className='flex gap-8'>
+        <div>Home</div>
+        <div>xxx</div>
+        <div>xxx</div>
+        <div>xxx</div>
+        <div>xxx</div>
+      </div>
+      <div>
+asdasdas
+      </div>
+    </div>
+  )
+}
