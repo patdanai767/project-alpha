1e51d51dad64757fbb71ed475e268a1db6d94db6
bowvanit <nnubow1304@gmail.com>
2024-11-30 20:44:30 +0700

    register

--- project-alpha/src/pages/Register/Register.jsx
+++ project-alpha/src/pages/Register/Register.jsx
@@ -3,9 +3,9 @@ import './Register.css'
 
 const Register = () => {
   return (
-    
+    <div class='flex items-center justify-center min-h-screen from-purple-900 via-indigo-800 to-indigo-500 bg-gradient-to-br'>
     <div class="max-w-lg mx-auto  bg-white dark:bg-gray-900 rounded-3xl shadow-md px-20 py-7 flex flex-col items-center"> 
-    
+      
       <h1 class="text-4xl font-bold text-center text-blue-900 dark:text-gray-200 mb-4">Create new Account</h1>
       <h2 className=" text-blue-900 mb-8 font-bold">Already Registered? Login</h2>
       <form action="#" class="w-full flex flex-col gap-4">
@@ -30,6 +30,7 @@ const Register = () => {
       
      
       </form>
+     </div>
      </div>
      
 )
