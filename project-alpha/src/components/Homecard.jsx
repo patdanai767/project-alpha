ad51a44d820f9d32011c0013b87f35850d041fde
phupn47 <phuphawadon@gmail.com>
2024-12-06 12:24:18 +0700

    update2 home

--- project-alpha/src/components/Homecard.jsx
+++ project-alpha/src/components/Homecard.jsx
@@ -1,17 +1,18 @@
 import { Target } from "lucide-react";
 import React from "react";
 
-const Homecard = () => {
+const Homecard = ({ name, image, category }) => {
   return (
     <div className=" text-white cursor-pointer hover:scale-105 hover:drop-shadow-lg transition duration-200 ease-in-out">
-      <div className="bg-[url('./src/assets/profile-pic.jpg')] rounded-xl bg-cover bg-center w-72 h-96 flex items-end">
-        <div className="flex flex-col items-start p-4 w-full bg-black backdrop-blur-sm bg-opacity-80 rounded-b-xl hover:bg-blue-800 hover:bg-opacity-80 transition duration-200 ease-in-out">
-          <p className="flex text-4xl font-bold mb-2 w-full truncate">
-            Shin Chan
+      <div className="w-60 h-full flex flex-col">
+        <img src={image} alt={name} className="w-60 h-60 rounded-t-xl" />
+        <div className="flex flex-col items-start px-3 py-2 w-full overflow-hidden bg-black bg-opacity-80 rounded-b-xl hover:bg-blue-800 hover:bg-opacity-80 transition duration-200 ease-in-out">
+          <p className="text-4xl text-left font-bold pb-2 w-full truncate whitespace-nowrap">
+            {name}
           </p>
           <div className="flex items-center gap-2 text-xl w-full">
             <Target />
-            <p className="text-xl truncate">Weight training</p>
+            <p className="text-xl truncate">{category}</p>
           </div>
         </div>
       </div>
--- project-alpha/src/components/Navbar.jsx
+++ project-alpha/src/components/Navbar.jsx
@@ -42,36 +42,43 @@ const Navbar = () => {
         </div>
       </div>
 
-      {isMenuOpen ? (
-        <div
-          onClick={toggleMenu}
-          className="fixed top-0 right-0 w-screen h-screen bg-black bg-opacity-25 z-40 "
-        >
-          <div className="fixed top-0 right-0 w-60 h-screen p-7 bg-yellow-50 text-black text-xl font-semibold z-50">
-            <div className="flex items-center justify-between border-b-2 border-blue-800 pb-7">
-              <a
-                href="/login"
-                className="flex items-center gap-2 cursor-pointer"
-              >
-                <User />
-                <p>Login</p>
-              </a>
-              <div
-                onClick={toggleMenu}
-                className="flex items-center cursor-pointer"
-              >
-                <X />
-              </div>
-            </div>
-            <div className="flex flex-col items-start">
-              <a href="/search" className="mt-8 cursor-pointer">
-                Choose trainers
-              </a>
-              <a className="mt-8 cursor-pointer">Become a trainer</a>
-            </div>
+      {/* sidebar overlay */}
+      <div
+        onClick={toggleMenu}
+        className={`fixed top-0 right-0 w-screen h-screen bg-black bg-opacity-25 z-40 transition ${
+          isMenuOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+      ></div>
+      {/* sidebar menu */}
+      <div
+        className={`fixed top-0 right-0 w-60 h-screen p-7 bg-yellow-50 text-black text-xl font-semibold z-50 transform transition-transform duration-200 ${
+          isMenuOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <div className="flex items-center justify-between border-b-2 border-blue-800 pb-7">
+          <a href="/login" className="flex items-center gap-2 cursor-pointer">
+            <User className="bg-blue-200 rounded-sm p-1" />
+            <p>Login</p>
+          </a>
+          <div
+            onClick={toggleMenu}
+            className="flex items-center cursor-pointer"
+          >
+            <X className="w-full h-full p-2 rounded-xl cursor-pointer hover:bg-blue-200" />
           </div>
         </div>
-      ) : null}
+        <div className="flex flex-col items-start">
+          <a
+            href="/search"
+            className="w-full h-full mt-4 p-2 rounded-xl cursor-pointer hover:bg-blue-200"
+          >
+            Choose trainers
+          </a>
+          <a className="w-full h-full mt-4 p-2 rounded-xl cursor-pointer hover:bg-blue-200">
+            Become a trainer
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
