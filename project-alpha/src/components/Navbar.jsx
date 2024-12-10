ca08fa4bb936f712c6d02d7a36ad78ed69705cd8
phupn47 <phuphawadon@gmail.com>
2024-12-10 21:09:09 +0700

    update home final monk

--- project-alpha/src/components/Navbar.jsx
+++ project-alpha/src/components/Navbar.jsx
@@ -9,19 +9,19 @@ const Navbar = () => {
   };
 
   return (
-    <div className="bg-blue-800 text-white w-full flex items-center justify-between p-7 sm:p-6">
+    <div className="bg-blue text-sky font-montserrat w-full flex items-center justify-between p-7 sm:p-6">
       <div className="flex items-center gap-8">
-        <p className="text-lg font-bold cursor-pointer hover:text-cyan-300 transition duration-200 ease-in-out">
+        <p className="text-lg font-bold cursor-pointer hover:text-lime transition duration-200 ease-in-out">
           PROJECT ALPHA
         </p>
         <div className="hidden sm:flex gap-8 cursor-pointer">
           <a
             href="/search"
-            className="cursor-pointer hover:text-cyan-300 transition duration-200 ease-in-out"
+            className="cursor-pointer hover:text-lime transition duration-200 ease-in-out"
           >
             Choose trainers
           </a>
-          <p className="cursor-pointer hover:text-cyan-300 transition duration-200 ease-in-out">
+          <p className="cursor-pointer hover:text-lime transition duration-200 ease-in-out">
             Become a trainer
           </p>
         </div>
@@ -29,14 +29,14 @@ const Navbar = () => {
       <div>
         <a
           href="/login"
-          className="hidden sm:flex items-center gap-2 border-2 rounded-xl cursor-pointer px-4 py-1 hover:text-cyan-300 hover:border-cyan-300 transition duration-200 ease-in-out"
+          className="hidden sm:flex items-center gap-2 border-2 rounded-xl cursor-pointer px-4 py-1 hover:text-lime hover:border-lime transition duration-200 ease-in-out"
         >
           <LogIn />
           <div className="font-semibold ">Login</div>
         </a>
         <div
           onClick={toggleMenu}
-          className="flex sm:hidden cursor-pointer hover:text-cyan-300 hover:border-cyan-300 transition duration-200 ease-in-out"
+          className="flex sm:hidden cursor-pointer hover:text-lime hover:border-lime transition duration-200 ease-in-out"
         >
           <Menu />
         </div>
@@ -51,30 +51,37 @@ const Navbar = () => {
       ></div>
       {/* sidebar menu */}
       <div
-        className={`fixed top-0 right-0 w-60 h-screen p-7 bg-yellow-50 text-black text-xl font-semibold z-50 transform transition-transform duration-200 ${
+        className={`fixed top-0 right-0 w-60 h-screen p-7 bg-sky text-blue text-xl font-semibold z-50 transform transition-transform duration-200 ${
           isMenuOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
-        <div className="flex items-center justify-between border-b-2 border-blue-800 pb-7">
+        <div className="flex items-center justify-between border-b-2 border-blue pb-7">
           <a href="/login" className="flex items-center gap-2 cursor-pointer">
-            <User className="bg-blue-200 rounded-sm p-1" />
+            <User className="bg-lime rounded-sm p-1" />
             <p>Login</p>
           </a>
           <div
             onClick={toggleMenu}
             className="flex items-center cursor-pointer"
           >
-            <X className="w-full h-full p-2 rounded-xl cursor-pointer hover:bg-blue-200" />
+            <X
+              className="w-full h-full p-2 rounded-xl cursor-pointer hover:bg-lime
+            "
+            />
           </div>
         </div>
         <div className="flex flex-col items-start">
           <a
             href="/search"
-            className="w-full h-full mt-4 p-2 rounded-xl cursor-pointer hover:bg-blue-200"
+            className="w-full h-full mt-4 p-2 rounded-xl cursor-pointer hover:bg-lime
+            "
           >
             Choose trainers
           </a>
-          <a className="w-full h-full mt-4 p-2 rounded-xl cursor-pointer hover:bg-blue-200">
+          <a
+            className="w-full h-full mt-4 p-2 rounded-xl cursor-pointer hover:bg-lime
+          "
+          >
             Become a trainer
           </a>
         </div>
--- project-alpha/tailwind.config.js
+++ project-alpha/tailwind.config.js
@@ -6,8 +6,16 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        blue: "#2F4ACC",
+        lightblue: "#4A8BDF",
+        sky: "#EFFAFD",
+        lime: "#DDF344",
+        green: "#38A32A",
+        white: "#FFFFFF"
+      },
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
       },
     },
   },
