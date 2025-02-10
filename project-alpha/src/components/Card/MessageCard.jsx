b3d731c09e92d607a44b3caa5255d033c61f8162
patdanai767 <pattproc@gmail.com>
2025-02-10 19:11:28 +0700

    feat:messenger page

--- project-alpha/src/components/Card/MessageCard.jsx
+++ project-alpha/src/components/Card/MessageCard.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import { motion } from "framer-motion";
 
 export default function MessageCard() {
   return (
-    <div className='border-b border-blue flex gap-[16px] items-center cursor-pointer'>
-        <div>Profile</div>
-        <div className='flex-1 gap-[8px] mb-[8px]'>
-            <div className='justify-between flex'>
-                <div className='text-[20px] font-semibold'>Trainer A</div>
-                <div>22 Dec</div>
-            </div>
-            <div>Hello, user...</div>
+    <motion.div
+      animate={{
+        y: 0,
+        opacity: 1,
+      }}
+      initial={{
+        y: 20,
+        opacity: 0,
+      }}
+      className="border-b border-blue flex gap-[16px] items-center cursor-pointer"
+    >
+      <div>Profile</div>
+      <div className="flex-1 gap-[8px] mb-[8px]">
+        <div className="justify-between flex">
+          <div className="text-[20px] font-semibold">Trainer A</div>
+          <div>22 Dec</div>
         </div>
-    </div>
-  )
+        <div>Hello, user...</div>
+      </div>
+    </motion.div>
+  );
 }
