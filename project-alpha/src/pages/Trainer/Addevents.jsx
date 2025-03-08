193224f253db3bcf7334e17b1e8c50aa5efdb3ac
Patrdanai Chumrus <pattproc@gmail.com>
2025-03-08 22:34:56 +0700

    feat:api events

--- project-alpha/src/pages/Trainer/Addevents.jsx
+++ project-alpha/src/pages/Trainer/Addevents.jsx
@@ -11,14 +11,30 @@ import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 
 export default function Addevents() {
-
   const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [data, setData] = useState({
+    title: "",
+    description: "",
+    trainee: "",
+    startedAt: "",
+    endAt: "",
+  });
 
   const navigate = useNavigate();
 
   const handleBack = () => {
     navigate("/dashboard");
-  }
+  };
+
+  const handleChange = (e) => {
+    setData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = (e) => {
+    const payload = { ...data, startedAt: startDate, endAt: endDate };
+    console.log(payload);
+  };
 
   return (
     <div className="flex justify-center">
@@ -50,29 +66,42 @@ export default function Addevents() {
         </div>
       </div>
       <div className="min-h-screen w-3/4 m-16">
-      <div className="hidden md:block">
-        <div className="text-[36px] font-semibold">Welcome, Trainee</div>
-        <div className="my-[32px]">
-          <div className="text-[24px] my-[32px]">Upcoming course</div>
-          <EventCardTrainer />
-        </div>
+        <div className="hidden md:block">
+          <div className="text-[36px] font-semibold">Welcome, Trainee</div>
+          <div className="my-[32px]">
+            <div className="text-[24px] my-[32px]">Upcoming course</div>
+            <EventCardTrainer />
+          </div>
         </div>
         <div className="mb-[32px]">
-        <div className="flex items-center gap-4"><ChevronLeft onClick={handleBack} className="flex cursor-pointer size-6"/>
-          <div className="font-semibold text-[24px] my-[32px]">Add events</div></div>
+          <div className="flex items-center gap-4">
+            <ChevronLeft
+              onClick={handleBack}
+              className="flex cursor-pointer size-6"
+            />
+            <div className="font-semibold text-[24px] my-[32px]">
+              Add events
+            </div>
+          </div>
           <div className="flex justify-between gap-8">
             <div className="w-full">
               <p className="text-[20px] font-medium mb-1">Title</p>
               <input
+                onChange={handleChange}
                 placeholder="Meeting"
                 type="text"
-                name="username"
+                name="title"
                 className="p-[12px] rounded-xl border border-lightblue bg-transparent w-full text-black placeholder-gray"
               />
             </div>
             <div className="w-full">
               <p className="text-[20px] font-medium mb-1">Trainee</p>
-              <select className="p-[12px] rounded-xl border border-lightblue bg-transparent w-full">
+              <select
+                name="trainee"
+                onChange={handleChange}
+                className="p-[12px] rounded-xl border border-lightblue bg-transparent w-full"
+              >
+                
                 <option>a</option>
                 <option>b</option>
                 <option>c</option>
@@ -82,7 +111,11 @@ export default function Addevents() {
           </div>
           <div className="mt-[32px]">
             <p className="text-[20px] font-medium mb-1">Description</p>
-            <textarea className="p-[12px] rounded-xl border border-lightblue bg-transparent w-full overflow-x-scroll" />
+            <textarea
+              name="description"
+              onChange={handleChange}
+              className="p-[12px] rounded-xl border border-lightblue bg-transparent w-full overflow-x-scroll"
+            />
           </div>
           <div className="flex mt-[32px] gap-8">
             <div className="w-full">
@@ -100,10 +133,18 @@ export default function Addevents() {
               <DatePicker
                 showTimeSelect
                 dateFormat="Pp"
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
+                selected={endDate}
+                onChange={(date) => setEndDate(date)}
                 className="text-center cursor-pointer p-[10px] rounded-xl border border-lightblue bg-transparent w-full"
               />
+            </div>
+          </div>
+          <div className="flex justify-end mt-6">
+            <div
+              onClick={handleClick}
+              className="p-2 border-2 h-[50px] w-[200px] grid place-items-center rounded-[12px] bg-blue text-white hover:cursor-pointer"
+            >
+              Save
             </div>
           </div>
         </div>
