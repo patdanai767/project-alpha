c128fc0e450d9bdf443f16a9798eba832b541b21
patdanai767 <pattproc@gmail.com>
2025-03-30 20:12:17 +0700

    feat: like button change url

--- src/components/Button/HeartButton.jsx
+++ src/components/Button/HeartButton.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 
 const HeartButton = ({ courseId, isLike }) => {
   const [liked, setLiked] = useState(isLike || false);
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
   useEffect(() => {
     fetchLike();
