7318fb963d1c955d85d2dcb2f4abc82bc97bf87c
patdanai767 <pattproc@gmail.com>
2025-03-30 19:13:05 +0700

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
         "/api": {
           target: env.VITE_API_BASE_URL,
           changeOrigin: true,
-          secure: false,
+          secure: true,
           rewrite: path => path.replace('/api', ''),
         },
       },
